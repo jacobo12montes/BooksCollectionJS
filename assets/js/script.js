46d9fc648c994ffa470b8ff4e6b576daeb95fd88
jacobo12montes <57403326+jacobo12montes@users.noreply.github.com>
2021-08-10 14:12:55 -0500

    fix some linter issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,28 @@
 class Book {
-    constructor() {
-      this.bookArray = Object.entries(window.localStorage);
-      this.html = [];
-    }
-  
-    addBooks() {
-      this.bookName = document.getElementById('name-book').value;
-      this.bookAuthor = document.getElementById('author-book').value;
-      localStorage.setItem(this.bookName, this.bookAuthor);
-    }
-  
-    display() {
-      this.bookArray.forEach(([key, value]) => {
-        this.html.push(`<div class='book'><div class='text-container'><p class='book-title '>${key} by</p><p class='book-author'>&nbsp;${value}</p></div><button class='remove offset-md-1' onclick="book.removeBooks('${key}')">Remove</button></div>`);
-      });
-      document.getElementById('book-list').innerHTML = this.html.join(' ');
-    }
-  
-    removeBooks(i) {
-      localStorage.removeItem(i);
-      this.bookArray = Object.entries(window.localStorage);
-      window.location.reload();
-    }
+  constructor() {
+    this.bookArray = Object.entries(window.localStorage);
+    this.html = [];
   }
-  
-  const book = new Book();
-  book.display();
+
+  addBooks() {
+    this.bookName = document.getElementById('name-book').value;
+    this.bookAuthor = document.getElementById('author-book').value;
+    localStorage.setItem(this.bookName, this.bookAuthor);
+  }
+
+  display() {
+    this.bookArray.forEach(([key, value]) => {
+      this.html.push(`<div class='book'><div class='text-container'><p class='book-title '>${key} by</p><p class='book-author'>&nbsp;${value}</p></div><button class='remove offset-md-1' onclick="book.removeBooks('${key}')">Remove</button></div>`);
+    });
+    document.getElementById('book-list').innerHTML = this.html.join(' ');
+  }
+
+  removeBooks(i) {
+    localStorage.removeItem(i);
+    this.bookArray = Object.entries(window.localStorage);
+    window.location.reload();
+  }
+}
+
+const book = new Book();
+book.display();
