4cc374dc6a71b4d2c76e3280565b509e4cd405be
jacobo12montes <57403326+jacobo12montes@users.noreply.github.com>
2021-08-10 15:41:21 -0500

    Change the class for functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,30 @@
-class Book {
-  constructor() {
-    this.bookArray = Object.entries(window.localStorage);
-    this.html = [];
-  }
+let bookArray = [];
+const html = [];
+const myStorage = window.localStorage;
+bookArray = Object.entries(myStorage);
 
-  addBooks() {
-    this.bookName = document.getElementById('name-book').value;
-    this.bookAuthor = document.getElementById('author-book').value;
-    localStorage.setItem(this.bookName, this.bookAuthor);
-  }
-
-  display() {
-    this.bookArray.forEach(([key, value]) => {
-      this.html.push(`<div class='book'><div class='text-container'><p class='book-title '>${key} by</p><p class='book-author'>&nbsp;${value}</p></div><button class='remove offset-md-1' onclick="book.removeBooks('${key}')">Remove</button></div>`);
-    });
-    document.getElementById('book-list').innerHTML = this.html.join(' ');
-  }
-
-  removeBooks(i) {
-    localStorage.removeItem(i);
-    this.bookArray = Object.entries(window.localStorage);
-    window.location.reload();
-  }
+// eslint-disable-next-line no-unused-vars
+function addBooks() {
+  const bookName = document.getElementById('name-book').value;
+  const bookAuthor = document.getElementById('author-book').value;
+  localStorage.setItem(bookName, bookAuthor);
 }
 
-const book = new Book();
-book.display();
+function removeBooks(i) {
+  const title = i.firstChild.innerText;
+  Object.fromEntries(bookArray);
+  localStorage.removeItem(title);
+  bookArray = Object.entries(myStorage);
+  i.remove();
+}
+
+bookArray.forEach(([key, value]) => {
+  html.push(`<div class='book'><p class='book-title'>${key}</p><p class='book-author'>${value}</p><button class='remove'>Remove</button><br><br></div>`);
+});
+
+document.getElementById('book-list').innerHTML = html.join(' ');
+
+for (let i = 0; i < document.getElementsByClassName('remove').length; i += 1) {
+  const bookTitle = document.getElementsByClassName('remove')[i].parentElement;
+  document.getElementsByClassName('remove')[i].addEventListener('click', () => { removeBooks(bookTitle); });
+}
