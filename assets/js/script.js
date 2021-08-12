1e5d24ce422dfd677458fb8baf7281f320c09dc0
jacobo12montes <57403326+jacobo12montes@users.noreply.github.com>
2021-08-11 19:24:15 -0500

    Make the books with the same title posible

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,16 @@ class Book {
   }
 
   addBooks() {
+    this.id = document.getElementById('name-book').value.concat(document.getElementById('author-book').value);
     this.bookName = document.getElementById('name-book').value;
     this.bookAuthor = document.getElementById('author-book').value;
-    localStorage.setItem(this.bookName, this.bookAuthor);
+    localStorage.setItem(this.id, [this.bookName, this.bookAuthor]);
   }
 
   display() {
     this.bookArray.forEach(([key, value]) => {
-      this.html.push(`<div class='book'><div class='text-container'><p class='book-title '>${key} by</p><p class='book-author'>&nbsp;${value}</p></div><button class='remove offset-md-1' onclick="book.removeBooks('${key}')">Remove</button></div>`);
+      const array = value.split(',');
+      this.html.push(`<div class='book'><div class='text-container'><p class='book-title '>${array[0]} by</p><p class='book-author'>&nbsp;${array[1]}</p></div><button class='remove offset-md-1' onclick="book.removeBooks('${key}')">Remove</button></div>`);
     });
     document.getElementById('book-list').innerHTML = this.html.join(' ');
   }
